249d321327cca2eb0a0ac57becee8d3fa61b897b
fiorenzo pizza <fiorenzino@gmail.com>
2017-12-10 14:58:29 +0100

    more test classes

--- src/test/java/nz/fiore/vertx/ext/jpa/actions/JPAQueryTest.java
+++ src/test/java/nz/fiore/vertx/ext/jpa/actions/JPAQueryTest.java
@@ -1,9 +1,12 @@
 package nz.fiore.vertx.ext.jpa.actions;
 
+import io.vertx.core.json.JsonObject;
 import io.vertx.ext.unit.Async;
 import io.vertx.ext.unit.TestContext;
 import io.vertx.ext.unit.junit.VertxUnitRunner;
 import nz.fiore.vertx.ext.jpa.AbstractBaseTest;
+import nz.fiore.vertx.ext.jpa.sql.JPAConnection;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -15,8 +18,24 @@ public class JPAQueryTest extends AbstractBaseTest
    public void query(TestContext context)
    {
       Async async = context.async();
-
-      async.complete();
+      jpaClient.getJPAConnection(conn -> {
+         JPAConnection connection = conn.result();
+         connection.create(CREATE_TABLE, result -> {
+            System.out.println("create table");
+            Assert.assertTrue(result.succeeded());
+            connection.persist(TABLE, whiskyP.toJson(), result_p -> {
+               System.out.println("persist");
+               Assert.assertTrue(result_p.succeeded());
+               connection.query("selct * from " + TABLE + " where name = :NAME ",
+                        new JsonObject().put("NAME", whiskyU.name), result_q-> {
+                           System.out.println("merge");
+                           Assert.assertTrue(result_q.succeeded());
+                           Assert.assertEquals(result_q.result().getRows(), 1);
+                           async.complete();
+                        });
+            });
+         });
+      });
    }
 
    @Test
